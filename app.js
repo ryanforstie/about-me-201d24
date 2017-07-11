a3427a85c1c228e8c55155aded7d52eb56ac0d4f
ryanforstie <ryanforstie@gmail.com>
2017-07-11 15:20:18 -0700

    done with html,css

---
+++ app.js
@@ -0,0 +1,16 @@
+
+ var username = prompt('What is your name');
+alert('Nice to meet you, ' + username + '. Thanks for stopping by.');
+console.log('The user said their name is ' + username);
+
+var hometown = prompt('Where are you from?');
+alert('Nice to meet you, ' + username + ' from ' + hometown);
+console.log('The user said they are from ' + hometown);
+
+var age = prompt('How old are you?');
+alert(age, + ' Wow ' + username + ' from ' + hometown + ' you are old!');
+console.log('The user said they are ' + age);
+
+var runSpeed = prompt('How fast can you run?');
+alert(runSpeed + ' Thats really slow, just kidding ' + username + ' from ' + hometown + ' your super fast and look good for ' + age);
+console.log('The user said they can run ' + runSpeed);
